e53a8260cd75b350657d20476bdb6cca9da09ab1
Luis Juarez <luisjuarezt16@gmail.com>
2025-12-02 13:17:31 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/NavegaLasRutas---Juarez/">
     <App />
   </BrowserRouter>
 )
